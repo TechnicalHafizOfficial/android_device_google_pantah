3903b9d4d195bb3ed50cdf3907f36deb946128d3
technical hafiz <83238435+TechnicalHafizOfficial@users.noreply.github.com>
2026-02-12 10:35:04 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,4 +7,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_cheetah.mk \
     $(LOCAL_DIR)/aosp_panther.mk \
     $(LOCAL_DIR)/lineage_cheetah.mk \
-    $(LOCAL_DIR)/lineage_panther.mk
+    $(LOCAL_DIR)/infinity_pather.mk
